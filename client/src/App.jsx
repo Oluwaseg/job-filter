32c0185c0052b50727fa9278142fc8c86a56a4c7
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-06-07 14:26:31 +0100

    modifying App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import {
   Route,
   createBrowserRouter,
@@ -6,101 +5,58 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Homepage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage from "./pages/JobPage";
-import JobLoader from "./components/JobLoader";
 import AddJobPage from "./pages/AddJobPage";
 import EditJobPage from "./pages/EditJobPage";
+import JobLoader from "./components/JobLoader";
 
 const App = () => {
-  const [jobs, setJobs] = useState([]);
-  const fetchJobs = async () => {
-    try {
-      const res = await fetch("https://job-filter-ncb8.onrender.com/api/jobs");
-      if (!res.ok) {
-        throw new Error("Failed to fetch jobs");
-      }
-      const data = await res.json();
-      setJobs(data);
-    } catch (error) {
-      console.error("Error fetching jobs:", error);
-    }
+  // Add New Job
+  const addJob = async (newJob) => {
+    const res = await fetch("https://job-filter-ncb8.onrender.com/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return res.json;
   };
 
-  useEffect(() => {
-    // Fetch jobs data when the component mounts
-    fetchJobs();
-  }, []); // Empty dependency array ensures this effect runs only once, when the component mounts
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(
+      `https://job-filter-ncb8.onrender.com/api/jobs/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    return res.json;
+  };
 
-  const addJob = async (newJob) => {
-    try {
-      const res = await fetch("https://job-filter-ncb8.onrender.com/api/jobs", {
-        method: "POST",
+  // Update Job
+  const updateJob = async (job) => {
+    const res = await fetch(
+      `https://job-filter-ncb8.onrender.com/api/jobs/${job.id}`,
+      {
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(newJob),
-      });
-      if (!res.ok) {
-        throw new Error("Failed to add job");
+        body: JSON.stringify(job),
       }
-      // After adding job, fetch updated jobs data
-      fetchJobs();
-    } catch (error) {
-      console.error("Error adding job:", error);
-      throw error;
-    }
-  };
-
-  const deleteJob = async (id) => {
-    try {
-      const res = await fetch(
-        `https://job-filter-ncb8.onrender.com/api/jobs/${id}`,
-        {
-          method: "DELETE",
-        }
-      );
-      if (!res.ok) {
-        throw new Error("Failed to delete job");
-      }
-      // After deleting job, fetch updated jobs data
-      fetchJobs();
-    } catch (error) {
-      console.error("Error deleting job:", error);
-      throw error;
-    }
-  };
-
-  const updateJob = async (job) => {
-    try {
-      const res = await fetch(
-        `https://job-filter-ncb8.onrender.com/api/jobs/${job.id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(job),
-        }
-      );
-      if (!res.ok) {
-        throw new Error("Failed to update job");
-      }
-      // After updating job, fetch updated jobs data
-      fetchJobs();
-    } catch (error) {
-      console.error("Error updating job:", error);
-      throw error;
-    }
+    );
+    return res.json;
   };
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />}>
-        <Route index element={<Homepage />} />
-        <Route path="/jobs" element={<JobsPage jobs={jobs} />} />
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobsPage />} />
         <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
         <Route
           path="/edit-job/:id"
@@ -119,5 +75,4 @@ const App = () => {
 
   return <RouterProvider router={router} />;
 };
-
 export default App;
